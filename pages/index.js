893c807a8679c744a3e6bd4c53feab03361e10e0
TheTruUchiha <75875192+TheTruUchiha@users.noreply.github.com>
2021-09-25 17:42:12 +0530

    new title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home({results}) {
   return (
     <div style={{backgroundColor: '#06202A'}}>
       <Head>
-        <title>Create Next App</title>
+        <title>HuluLite - Stream your favourite shows.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
